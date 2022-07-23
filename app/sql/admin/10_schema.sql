9b1ac355c363050d99d3bb5805ecc408d2798803
mazrean <shunsuke.w04@gmail.com>
2022-07-23 13:10:46 +0900

    fix app

--- app/sql/admin/10_schema.sql
+++ app/sql/admin/10_schema.sql
@@ -29,3 +29,11 @@ CREATE TABLE `visit_history` (
   `updated_at` BIGINT NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 create index idx_visit_history_tenant_id_competition_id ON `visit_history`(`tenant_id`,`competition_id`);
+
+CREATE TABLE `visit_history2` (
+  `player_id` VARCHAR(255) NOT NULL,
+  `tenant_id` BIGINT UNSIGNED NOT NULL,
+  `competition_id` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL,
+  PRIMARY KEY (`tenant_id`, `competition_id`, `player_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
