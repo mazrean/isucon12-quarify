29c718eb6eef85d3a58d0dc757415a32719d0a86
mazrean <shunsuke.w04@gmail.com>
2022-07-23 15:26:41 +0900

    log

---
+++ app/sql/load.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
+ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
+ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
+ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
+ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
+
+for db in $( ls ../../initial_data/*.db ); do
+    ./sqlite3-to-sql ${db} | mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME"
+done
